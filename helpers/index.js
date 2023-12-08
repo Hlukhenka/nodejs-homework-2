330f8db666564c554e4a21b32b7753d71b125aeb
Hlukhenka <anastasiagluhenka@gmail.com>
2023-12-08 14:14:29 +0200

    +

--- helpers/index.js
+++ helpers/index.js
@@ -2,8 +2,11 @@
 
 const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
+const HandleMongooseError = require("./HandleMongooseError")
+
 
 module.exports = {
   HttpError,
   ctrlWrapper,
+  HandleMongooseError,
 };
--- middlewares/index.js
+++ middlewares/index.js
@@ -2,8 +2,10 @@
 
 const validateBody = require("./validateBody");
 const isValid = require("./isValidId");
+const authenticate = require("./authenticate"); 
 
 module.exports = {
   validateBody,
   isValid,
+  authenticate,
 };
